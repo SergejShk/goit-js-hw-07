4d2a9677d3aebc19214c9e8e324a8274ac3b5905
SergejShk <sshkurupij11@gmail.com>
2022-06-22 00:47:20 +0300

    add 2 tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const makeGalleryMurkup = (objectItems) => {
+  return objectItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                />
+                </a>
+            </div>`;
+    })
+    .join("");
+};
+
+const galleryBox = document.querySelector(".gallery");
+
+const imageMarkup = makeGalleryMurkup(galleryItems);
+galleryBox.insertAdjacentHTML("beforeend", imageMarkup);
+
+const onGetOfLargestImage = (evt) => {
+  evt.preventDefault();
+  const imageEl = evt.target;
+
+  if (imageEl.nodeName !== "IMG") {
+    return;
+  }
+  const imageAttribute = imageEl.getAttribute("data-source");
+  imageEl.src = imageAttribute;
+
+  const modal = basicLightbox.create(`
+    <img src="${imageEl.src}" width="800" height="600">`);
+  modal.show(() => {
+    window.addEventListener("keydown", (evt) => {
+      const isEscKey = evt.code === "Escape";
+
+      if (isEscKey) {
+        modal.close();
+      }
+    });
+  });
+};
+
+galleryBox.addEventListener("click", onGetOfLargestImage);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const createImageMarkup = (objectItems) => {
+  return objectItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" />
+        </a>
+        `;
+    })
+    .join("");
+};
+
+const galleryListRef = document.querySelector(".gallery");
+const imageMarkup = createImageMarkup(galleryItems);
+
+galleryListRef.insertAdjacentHTML("beforeend", imageMarkup);
+
+const onClickImage = (evt) => {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+};
+new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: "250" });
+
+galleryListRef.addEventListener("click", onClickImage);
